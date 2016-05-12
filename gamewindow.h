869f035f59a0f5564725b7e874ade7c5aeacdcf7
Anderson Luís Furlan <alsfurlan@gmail.com>
2016-05-12 18:04:54 -0300

    Desenhando um cubo

--- gamewindow.h
+++ gamewindow.h
@@ -3,14 +3,32 @@
 
 typedef struct gameWindow {
 	int running;	
+	GLfloat height;
+	GLfloat width;
 } GameWindow;
 
-void gameWindowRender() {
+void gameWindowInit(GameWindow g) 
+{
+	glClearColor(1.0f,1.0f,1.0f,1.0f);
+}
+
+void gameWindowRender() 
+{
 	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glVertex2f(	0.5f, 0.5f);
+		glVertex2f(-0.5f, 0.5f);
+	glEnd();
+	
 	glfwSwapBuffers();
 }
 
-void gameWindowUpdate() {
+void gameWindowUpdate() 
+{
 	
 }
 
--- main.c
+++ main.c
@@ -5,13 +5,18 @@ GameWindow gameWindow;
 
 int main(int argc, char **argv)
 {
+	gameWindow.running = 1; 
+	gameWindow.height = 600;
+	gameWindow.width  = 600;
+	
+	// Inicialização GLFW
 	glfwInit();
 	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
-	glfwOpenWindow(600,600,8,8,8,8,0,0,GLFW_WINDOW);
+	glfwOpenWindow(gameWindow.width,gameWindow.height,8,8,8,8,0,0,GLFW_WINDOW);
 	glfwSetWindowTitle("OpenGL Game");
-	glClearColor(1.0f,1.0f,1.0f,1.0f);
 	
-	gameWindow.running = 1; 
+	// Inicialização das configurações do game
+	gameWindowInit(gameWindow);
 	while(gameWindow.running) {
 		gameWindowRender();
 		gameWindowUpdate();
